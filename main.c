27c712568a9274a0c30c9413800092afadcc27e3
SergioOrozqui <78976723+SergioOrozqui@users.noreply.github.com>
2021-02-12 13:08:40 -0500

    Added I2C source code

--- main.c
+++ main.c
@@ -1,6 +1,37 @@
 #include "config.h"
 
+void I2Cinit(int BRG)
+{
+    I2C1BRG = BRG;
+    while(I2C1STATbits.P); //Check if bus is idle
+    I2C1CONbits.A10M = 0; //Select 7-bit address mode
+    I2C1CONbits.I2CEN = 1; //I2C Enable
+}
 
+void I2CStart(void)
+{
+    us_delay(20);
+    I2C1CONbits.SEN = 1; //Initiates start condition on SDAx and SCLx pins
+    while(I2C1CONbits.SEN); //Await start condition
+    us_delay(20);
+}
+
+void I2CStop(void)
+{
+    us_delay(20);
+    I2C1CONbits.PEN = 1; //Halts condition enable bit
+    while(I2C1CONbits.PEN); //Await stop conditon
+    us_delay(20);
+}
+
+char I2Cgetbyte(void)
+{
+    I2C1CONbits.RCEN = 1; //Enable I2C receive mode
+    while(!I2C1STATbits.RBF); //Wait for byte to shift into I2C1RCV register
+    I2C1CONbits.ACKEN = 1; //Master sends acknowledge
+    us_delay(20);
+    return (I2C1RCV);
+}
 
 void InitPWM(void) {
     T3CON = 0x8000; // enable TMR3, 1:1, 16 bit Timer, intclock

a8cb71cf9b8258c5510b359aa45354cc59ac2ed7
Brandon Bannavong <57028677+brandon2355@users.noreply.github.com>
2021-02-18 17:31:34 -0500

    Update main.c
    -Added servo positions
    Added ADC start function

--- main.c
+++ main.c
@@ -1,46 +1,67 @@
+/* August 22, 2019
+ * FILE: Lab5_PWM
+ * Start by copying code from Lecture8_PWM.
+ * The following is the code for the first part of Lab5.
+ * Changing "Value" will change the number of iterations PWM output stays constant.
+ * For larger Value, RC filter settles to constant voltage. 
+ */
+
 #include "config.h"
+
+#define SERVO_NEUTRAL 94
+#define SERVO_FORWARD 38
+#define SERVO_BACK 150
+void InitADC(int input)
+{
+    AD1CHS = input;    //select analog pins
+    AD1CON1 = 0x00E0;   //auto-convert mode
+    AD1CON2 = 0;        //using MUXA, AVss and AVdd as Vref
+    AD1CON3 = 0x1F01;   //Tad = 2 * Tcy = 125ns
+    AD1CSSL = 0;        //no scanning
+    AD1CON1bits.ADON = 1; //activate ADC 
+}
 
 void I2Cinit(int BRG)
 {
-    I2C1BRG = BRG;
-    while(I2C1STATbits.P); //Check if bus is idle
-    I2C1CONbits.A10M = 0; //Select 7-bit address mode
-    I2C1CONbits.I2CEN = 1; //I2C Enable
+    I2C2BRG = BRG;
+    while(I2C2STATbits.P); //Check if bus is idle
+    I2C2CONbits.A10M = 0; //Select 7-bit address mode
+    I2C2CONbits.I2CEN = 1; //I2C Enable
 }
 
 void I2CStart(void)
 {
     us_delay(20);
-    I2C1CONbits.SEN = 1; //Initiates start condition on SDAx and SCLx pins
-    while(I2C1CONbits.SEN); //Await start condition
+    I2C2CONbits.SEN = 1; //Initiates start condition on SDAx and SCLx pins
+    while(I2C2CONbits.SEN); //Await start condition
     us_delay(20);
 }
 
 void I2CStop(void)
 {
     us_delay(20);
-    I2C1CONbits.PEN = 1; //Halts condition enable bit
-    while(I2C1CONbits.PEN); //Await stop conditon
+    I2C2CONbits.PEN = 1; //Halts condition enable bit
+    while(I2C2CONbits.PEN); //Await stop conditon
     us_delay(20);
 }
 
 char I2Cgetbyte(void)
 {
-    I2C1CONbits.RCEN = 1; //Enable I2C receive mode
-    while(!I2C1STATbits.RBF); //Wait for byte to shift into I2C1RCV register
-    I2C1CONbits.ACKEN = 1; //Master sends acknowledge
+    I2C2CONbits.RCEN = 1; //Enable I2C receive mode
+    while(!I2C2STATbits.RBF); //Wait for byte to shift into I2C1RCV register
+    I2C2CONbits.ACKEN = 1; //Master sends acknowledge
     us_delay(20);
-    return (I2C1RCV);
+    return (I2C2RCV);
 }
 
 void I2Csendbyte(char data) {
-    while (I2C1STATbits.TBF); //wait if buffer is full
-    I2C1TRN = data; // pass data to transmission register
+    while (I2C2STATbits.TBF); //wait if buffer is full
+    I2C2TRN = data; // pass data to transmission register
     us_delay(10); // delay to be safe
 }
 void us_delay(int i)
 {
-    T1CON = 0x0810; //TMR1 on, 8:1 pre scale
+    T1CON = 0x0810; //TMR1 on, 1:8 pre scale
     TMR1 = 0;
     while(TMR1 < i*2)
     {
@@ -48,25 +69,34 @@ void us_delay(int i)
 }
 
 void InitPWM(void) {
-    T3CON = 0x8000; // enable TMR3, 1:1, 16 bit Timer, intclock
-    PR3 = 800 - 1;  // 20 kHz for Lab5. see Equ. 14-1 of data sheet, Fcy=16MHz.
+    T3CON = 0x8030; // enable TMR3, 1:256, 16 bit Timer, intclock
+    PR3 = 1250 - 1;  // 20 kHz for Lab5. see Equ. 14-1 of data sheet, Fcy=16MHz.
     _T3IF = 0;      // clear interrupt flag
     _T3IE = 1;      // enable TMR3 interrupt
-    OC1R = OC1RS = 400;     // initat 50% duty cycle
+    OC1R = OC1RS = SERVO_FORWARD;     // initat 50% duty cycle
                             // OC1R also loaded since first time.
     OC1CON = 0x000E;    // OCTSEL = 1 for Timer3, OCM<2:0> =110 for PWM mode
 } // InitAudio
 
 void _ISRFAST _T3Interrupt( void)
 {
-   OC1RS = 400;		// Count is set in main()                // increment k each Timer interrupt
+   OC1RS = SERVO_BACK;		// Count is set in main()                // increment k each Timer interrupt
    _T3IF = 0;      // clear interrupt flag and exit
 } // T3 Interrupt
-
+void ADCStart()
+{
+    AD1CON1bits.SAMP  = 1;
+    while(!AD1CON1bits.DONE);
+    AD1CON1bits.DONE = 0;
+    
+}
 int main (void)
 {
     InitPWM();
-    I2Cinit(0x9D);
+//    I2Cinit(0x9D); //enable I2C
+//    I2CStart(); // initiate start condition
+//    I2Csendbyte(0x64); //begin communication with slave
+//    I2CStop(); // halt condition enable bit
     while(1);
     return 0;
 }

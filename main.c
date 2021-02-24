a211c542e2a22f1e75150bb626eaa06ebba58b3d
Brandon Bannavong <57028677+brandon2355@users.noreply.github.com>
2021-02-24 17:32:37 -0500

    Update main.c
    Added stepper motor positions

--- main.c
+++ main.c
@@ -24,12 +24,15 @@ struct item
 #define SERVO_BACK 150  
 #define STEP_FULL_TURN 200
 #define STEP_QUART 50
+#define STEP_THREEQ 150
+#define STEP_TIME_DELAY 5//seconds
 #define RTS _RF13 // Output, For potential hardware handshaking.
 #define CTS _RF12 // Input, For potential hardware handshaking.
 int steps =0;
 int STEP_MOVE = 0;
 int SERVO_MOVE = 0;
 char* types = { "Plastic","Glass","Metal","Trash" };
+int dSec = 0, Sec = 0, Min = 0, Hr = 0;
 
 void InitADC(int input);
 void I2Cinit(int BRG);
@@ -41,12 +44,78 @@ void us_delay(int i);
 void InitServo(void);
 void _ISRFAST _T3Interrupt(void);
 void ADCStart();
-void MoveStepQuart(void)
+void StepPlastic();
+int StepCheckTime(int );
+void StepGlass()
+{
+    int curSec = Sec;
+    MoveStep180();
+    while(!StepCheckTime(curSec));
+    MoveStep180();
+}
+void StepTrash()
+{
+    int curSec = Sec;
+    MoveStep270();
+    while(!StepCheckTime(curSec));
+    MoveStep90();
+}
+void StepMetal()
+{
+    ;
+}
+void InitTime(void)
+{
+    _T4IP = 1;
+    TMR4 = 0;
+    T4CON = 0x8030;
+    PR4= 6250 - 1;
+    _T4IF = 0;
+    _T4IE = 1;
+
+}//InitTime
+
+void _ISR _T4Interrupt(void)
+{
+    dSec++;
+    if(dSec > 9)
+    {
+        dSec = 0;
+        Sec++;
+        if(Sec > 59)
+        {
+            Sec = 0;
+            Min++;
+            if(Min > 59)
+            {
+                Min = 0;
+                Hr++;
+                if(Hr > 24)
+                {
+                    Hr = 0;
+                }
+            }
+        }
+    }
+    _T4IF = 0;
+}
+void MoveStep90(void)
 {
     STEP_MOVE = 1;
     while(steps<STEP_QUART);
     STEP_MOVE = 0;
     steps = 0;
+}//MoveStepQuart
+void MoveStep270(void)
+{
+    MoveStep90();
+    MoveStep90();
+    MoveStep90();
+}
+void MoveStep180(void)
+{
+    MoveStep90();
+    MoveStep90();
 }
 void InitStepper()
 {
@@ -58,7 +127,7 @@ void InitStepper()
                             // OC1R also loaded since first time.
     OC2CON = 0x0006;    // OCTSEL = 0 for Timer2, OCM<2:0> =110 for PWM mode
     
-}
+}//InitStepper
 void _ISRFAST _T2Interrupt(void)
 {   
 
@@ -77,7 +146,7 @@ void _ISRFAST _T2Interrupt(void)
 void ms_delay(int N)
 {
     us_delay(1000 * N);
-}
+}//ms_delay
 void InitU2(void) {
     U2BRG = 34; // PIC24FJ128GA010 data sheet, 17.1 for calculation, Fcy= 16MHz.
     U2MODE = 0x8008; // See data sheet, pg148. Enable UART2, BRGH = 1,
@@ -86,15 +155,15 @@ void InitU2(void) {
     // Following lines pertain Hardware handshaking
     TRISFbits.TRISF13 = 1; // enable RTS , output
     RTS = 1; // default status , not ready to send
-}
-char putU2(char c) {
+}//InitU2
+char PutU2(char c) {
     while (CTS); //wait for !CTS (active low)
     while (U2STAbits.UTXBF); // Wait if transmit buffer full.
     U2TXREG = c; // Write value to transmit FIFO
     return c;
-}
+}//putU2
 
-char getU2(void) {
+char GetU2(void) {
     RTS = 0; // telling the other side !RTS
     while (!U2STAbits.URXDA); // wait
     RTS = 1; // telling the other side RTS
@@ -105,6 +174,7 @@ int main(void)
 {
     int c;
     TRISD = 0xFFFF;
+    InitTime();
     InitServo();
     InitStepper();
     InitU2();
@@ -122,7 +192,9 @@ int main(void)
         }
         else if(PORTDbits.RD7 == 0)
         {
-            MoveStepQuart();
+            //MoveStepQuart();
+            StepTrash();
+            
         }
         else
         {
@@ -131,26 +203,14 @@ int main(void)
         }
         for(c = 0; c<5;c++)
         {
-            putU2(message[c]);
+            PutU2(message[c]);
         }
        
         ms_delay(10);
 
     }
     return 0;
-}
-
-
-
-void InitADC(int input)
-{
-    AD1CHS = input;    //select analog pins
-    AD1CON1 = 0x00E0;   //auto-convert mode
-    AD1CON2 = 0;        //using MUXA, AVss and AVdd as Vref
-    AD1CON3 = 0x1F01;   //Tad = 2 * Tcy = 125ns
-    AD1CSSL = 0;        //no scanning
-    AD1CON1bits.ADON = 1; //activate ADC 
-}
+}//main
 
 void I2Cinit(int BRG)
 {
@@ -158,7 +218,7 @@ void I2Cinit(int BRG)
     while (I2C2STATbits.P); //Check if bus is idle
     I2C2CONbits.A10M = 0; //Select 7-bit address mode
     I2C2CONbits.I2CEN = 1; //I2C Enable
-}
+}//I2Cinit
 
 void I2CStart(void)
 {
@@ -166,7 +226,7 @@ void I2CStart(void)
     I2C2CONbits.SEN = 1; //Initiates start condition on SDAx and SCLx pins
     while (I2C2CONbits.SEN); //Await start condition
     us_delay(20);
-}
+}//I2CStart
 
 void I2CStop(void)
 {
@@ -174,7 +234,7 @@ void I2CStop(void)
     I2C2CONbits.PEN = 1; //Halts condition enable bit
     while (I2C2CONbits.PEN); //Await stop conditon
     us_delay(20);
-}
+}//I2CStop
 
 char I2Cgetbyte(void)
 {
@@ -183,13 +243,14 @@ char I2Cgetbyte(void)
     I2C2CONbits.ACKEN = 1; //Master sends acknowledge
     us_delay(20);
     return (I2C2RCV);
-}
+}//I2Cgetbyte
 
 void I2Csendbyte(char data) {
     while (I2C2STATbits.TBF); //wait if buffer is full
     I2C2TRN = data; // pass data to transmission register
     us_delay(10); // delay to be safe
-}
+}//I2Csendbyte
+
 void us_delay(int N)
 {
     T1CON = 0x8000;
@@ -198,7 +259,7 @@ void us_delay(int N)
     {
     }
     TMR1 = 0;
-}
+}//us_delay
 
 void InitServo(void) {
     T3CON = 0x8030; // enable TMR3, 1:256, 16 bit Timer, intclock
@@ -223,10 +284,33 @@ void _ISRFAST _T3Interrupt(void)
     }
     _T3IF = 0;      // clear interrupt flag and exit
 } // T3 Interrupt
+
 void ADCStart()
 {
     AD1CON1bits.SAMP = 1;
     while (!AD1CON1bits.DONE);
     AD1CON1bits.DONE = 0;
 
+}//ADCStart
+
+void InitADC(int input)
+{
+    AD1CHS = input;    //select analog pins
+    AD1CON1 = 0x00E0;   //auto-convert mode
+    AD1CON2 = 0;        //using MUXA, AVss and AVdd as Vref
+    AD1CON3 = 0x1F01;   //Tad = 2 * Tcy = 125ns
+    AD1CSSL = 0;        //no scanning
+    AD1CON1bits.ADON = 1; //activate ADC 
+}//InitADC
+
+int StepCheckTime(int curSec)
+{
+    return (Sec!= curSec&&Sec%STEP_TIME_DELAY - curSec%STEP_TIME_DELAY == 0);
+}
+void StepPlastic ()
+{
+    int curSec = Sec;
+    MoveStep90();
+    while(!StepCheckTime(curSec));
+    MoveStep270();
 }

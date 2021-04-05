6f55ea8e33ed2c9649d663a37f78ae0311eff22a
SergioOrozqui <78976723+SergioOrozqui@users.noreply.github.com>
2021-04-05 05:51:05 -0400

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,29 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import os
+IMG_PATH = os.getcwd()+"/test.jpg"
+class GUI:
+    def __init__(self):
+        self.ahter = 0
+
+def button():
+    print("button pressed")
+
+
+def main():
+    master = Tk()
+    master.title("Trashcan 1.0")
+    img = ImageTk.PhotoImage(Image.open(IMG_PATH))
+    #frame = Frame(master, height = 1000,width = 1000)
+    #frame.pack()
+    panel = Label(master,image = img)
+    butt = Button(master,text = "sddsf", command = lambda: button())
+    #panel.pack(side = RIGHT)
+    #butt.pack(side = LEFT)
+    panel.grid(row = 0, column = 1,sticky = W)
+    butt.grid(row = 0, column = 0,sticky = W)
+    master.mainloop()
+
+
+if __name__ == "__main__":
+    main()

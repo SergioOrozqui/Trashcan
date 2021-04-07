99f9ed32361e47499d11e67575f9faad9f1074f3
SergioOrozqui <78976723+SergioOrozqui@users.noreply.github.com>
2021-04-06 20:20:30 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,13 +1,19 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import os
-IMG_PATH = os.getcwd()+"/test.jpg"
+IMG_PATH = os.getcwd()+"/eval_img/eval.jpg"
+img = 0
 class GUI:
-    def __init__(self):
-        self.ahter = 0
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Trashcan 1.0")
+        self.detection_img = ImageTk.PhotoImage(Image.open(IMG_PATH))
 
-def button():
-    print("button pressed")
+
+    def button():
+        panel = Label(master,image = self.detection_img)
+        panel.grid(row = 0, column = 1,sticky = W)
+        print("button pressed")
 
 
 def main():
@@ -16,11 +22,11 @@ def main():
     img = ImageTk.PhotoImage(Image.open(IMG_PATH))
     #frame = Frame(master, height = 1000,width = 1000)
     #frame.pack()
-    panel = Label(master,image = img)
+    #panel = Label(master,image = img)
     butt = Button(master,text = "sddsf", command = lambda: button())
     #panel.pack(side = RIGHT)
     #butt.pack(side = LEFT)
-    panel.grid(row = 0, column = 1,sticky = W)
+    #panel.grid(row = 0, column = 1,sticky = W)
     butt.grid(row = 0, column = 0,sticky = W)
     master.mainloop()
 
